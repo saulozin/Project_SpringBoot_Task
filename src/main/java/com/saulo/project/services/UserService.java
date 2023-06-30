2e2022263661bb71530f15819b9991c196483fc9
Saulo Maia <saulomaia2@gmail.com>
2023-06-30 17:33:02 -0400

    Exception handling - findById

--- src/main/java/com/saulo/project/services/UserService.java
+++ src/main/java/com/saulo/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.saulo.project.entities.User;
 import com.saulo.project.repositories.UserRepository;
+import com.saulo.project.services.exceptions.ResourceNotFoundException;
 
 //Anotation para registrar a classe no mecanismo de injeção de dependência.
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepo.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
